9d42640fa8e695fdbd168d259b127eb132447ebb
Tomasz <dworowytomasz@gmail.com>
2019-05-02 19:17:55 +0200

    update

--- web_page/app.ts
+++ web_page/app.ts
@@ -1,13 +1,29 @@
 
-import * as WebRequest from 'web-request';
+//import * as WebRequest from 'web-request';  // don't work becous this import
+
+class HttpClient {
+    getRequest(aUrl:string, aCallback: any) {
+        var anHttpRequest = new XMLHttpRequest();
+        anHttpRequest.onreadystatechange = function() { 
+            if (anHttpRequest.readyState == 4 && anHttpRequest.status == 200)
+                aCallback(anHttpRequest.responseText);
+        }
+
+        anHttpRequest.open( "GET", aUrl, true );            
+        anHttpRequest.send( null );
+    }
+}
 
 class RandomString {
     public async render(divId: string) {
         let el: HTMLElement | null = document.getElementById(divId);
-        if(el != null){
-            el.innerText = 'WAITING'
-            var result = await WebRequest.get('http://localhost:3000'); // Don't work
-            el.innerText = result.content
+        {
+            var client = new HttpClient();
+            client.getRequest('http://localhost:3000', function(response:any) {
+                if(el != null) {
+                    el.innerText = response
+                }
+            }); // STILL DON'T WORK
         }
     }
 }
