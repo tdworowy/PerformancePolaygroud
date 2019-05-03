d609e509f3c30f3e35db116a4e45157405a44bac
Tomasz <dworowytomasz@gmail.com>
2019-05-03 22:33:20 +0200

    update

--- web_page/service.ts
+++ web_page/service.ts
@@ -1,15 +1,21 @@
 import { Utils } from "./utils";
-import http from 'http';
 import express from 'express';
 const app = express()
 const port = 3000
 
 app.get('/', (request:any, response:any) => {
-    console.log(`Request: ${request}`)
+    console.log(`Request GET: ${request}`)
     let text :string = Utils.StringUtils.generateString()
     response.setHeader("Access-Control-Allow-Origin", "*");
     response.setHeader("Access-Control-Allow-Methods", 'GET');
     response.send(text)
+  })
+
+app.post('/data', (request:any, response:any) => {
+    console.log(`Request POST: ${request}`)
+    response.setHeader("Access-Control-Allow-Origin", "*");
+    response.setHeader("Access-Control-Allow-Methods", 'POST');
+    response.send("OK")
   })
   
   app.listen(port, (err:any) => {
