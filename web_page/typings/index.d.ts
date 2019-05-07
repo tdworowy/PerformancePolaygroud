f32a4b0efaaba00cdae441f2e7694a7e29243042
Tomasz <dworowytomasz@gmail.com>
2019-05-07 16:46:36 +0200

    remove typings

--- web_page/typings/index.d.ts
+++
@@ -1,6 +0,0 @@
-/// <reference path="globals/node/index.d.ts" />
-/// <reference path="modules/chai/index.d.ts" />
-/// <reference path="modules/express-serve-static-core/index.d.ts" />
-/// <reference path="modules/express/index.d.ts" />
-/// <reference path="modules/mime/index.d.ts" />
-/// <reference path="modules/serve-static/index.d.ts" />
