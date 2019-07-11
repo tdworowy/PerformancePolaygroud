d4ac51296b0b83fdfcd8eedc901b435ae1bc799b
Tomasz <dworowytomasz@gmail.com>
2019-07-11 16:24:45 +0200

    update

--- web_page/server.ts
+++ web_page/server.ts
@@ -1,25 +1,14 @@
-var http = require('http');
-var fs = require('fs');
-
-const PORT=8081; 
-
-fs.readFile('./index.html', function (err:any, html:any) {
-    console.log(html);
-    if (err) {
-        console.log(err)
-        throw err;
-    }    
-
-    http.createServer(function(request:any, response:any) {  
-        response.writeHeader(200, {"Content-Type": "text/html"});  
-        response.writeHeader(200, {"Accept": "*/*"});  
-        response.writeHeader(200, {"Access-Control-Allow-Origin": "http://localhost"});  
-        response.writeHeader(200, {"Access-Control-Allow-Methods": "POST, GET, OPTIONS, DELETE"});  
-        response.write(html);  
-        response.end();
-        console.log(response);  
-    }).listen(PORT);
-    
+var connect = require('connect');
+var serveStatic = require('serve-static');
+var app = connect()
+    .use(serveStatic(__dirname))
+    .use(function(req:any, res:any){
+        console.log(req)
+        res.setHeader("Access-Control-Allow-Origin", "*");
+        res.setHeader("Access-Control-Allow-Methods", "GET");
+        res.setHeader("Access-Control-Allow-Headers", "accept, content-type");
+        
+    });
+app.listen(8081, function(){
+    console.log('Server running on 8081...');
 });
-// DON'T WORK
-// User server2
