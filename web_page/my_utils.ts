86cc39a28242fe8af1c0cef11705b081360f4ef3
Tomasz <dworowytomasz@gmail.com>
2019-07-15 07:40:44 +0200

    update

--- web_page/my_utils.ts
+++ web_page/my_utils.ts
@@ -3,9 +3,9 @@ export class StringUtils {
         static getRandomInt(min: number, max: number) {
             return Math.floor(Math.random() * (max - min)) + min;
         }
-        static generateString() {
+        static generateString(from:number=3000,to:number=10000) {
             var text: string="";
-            let num: number = StringUtils.getRandomInt(3000,10000)
+            let num: number = StringUtils.getRandomInt(from,to)
             for(var i:number = 0;i<=num;i++) {
                 text = text + uuid();
             }
--- web_page/service.ts
+++ web_page/service.ts
@@ -1,8 +1,12 @@
 import { StringUtils } from "./my_utils";
 import express from 'express';
+const uuid = require('uuid/v4');
 var bodyParser = require("body-parser");
 const app = express()
 const port = 3000
+
+let data:any = {}
+
 app.use(bodyParser.urlencoded({extended : true}));
 app.use(bodyParser.json())
 
@@ -13,8 +17,32 @@ app.get('/', (request:any, response:any) => {
     response.send(text)
     console.log(`Request GET`)
   })
+app.get('/data', (request:any, response:any) => {
+    
+    response.setHeader("Access-Control-Allow-Origin", "*");
+    response.setHeader("Access-Control-Allow-Methods", 'GET');
+    console.log(data)
+    response.json(data)
+    console.log(`Request GET Data`)
+  })
+app.get('/:key', (request:any, response:any) => {
+    
+    response.setHeader("Access-Control-Allow-Origin", "*");
+    response.setHeader("Access-Control-Allow-Methods", 'GET');
+    response.send(data[request.params.key])
+    console.log(`Request GET ${request.params.key}`)
+  })
+  app.put('/:key', (request:any, response:any) => {
+    
+    response.setHeader("Access-Control-Allow-Origin", "*");
+    response.setHeader("Access-Control-Allow-Methods", 'PUT');
+    data[request.params.key] = JSON.stringify(request.body)
+    response.send("Ok")
+    console.log(`Request PUT: ${JSON.stringify(request.body)}`)
+  })
 app.post('/data', (request:any, response:any) => {
     console.log(`Request POST: ${JSON.stringify(request.body)}`)
+    data[uuid()] = JSON.stringify(request.body)
     
     response.setHeader("Access-Control-Allow-Origin", "*");
     response.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
