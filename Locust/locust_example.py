461a2d3c4711dce16d6307f810b6a99dcdd6a572
Tomasz <dworowytomasz@gmail.com>
2019-07-24 13:58:28 +0200

    update

--- Locust/locust_example.py
+++ Locust/locust_example.py
@@ -3,7 +3,9 @@ import random
 import string
 import re
 from locust import HttpLocust, TaskSet, task
-
+import csv
+import json
+from os import  path
 
 def random_string(length: int) -> str:
     """
@@ -37,6 +39,25 @@ def put_data(self: TaskSet, data_size: int = 20):
     assert response.status_code is 200, "Unexpected response code: " + response.status_code
     assert response.elapsed < datetime.timedelta(seconds=2), "Request took more than 2 second"
 
+def post_data_csv(self: TaskSet):
+    """
+    :param self: TaskSet object.
+    """
+    with open(path.join("data","data.csv")) as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        rows = [row for row in csv_reader]
+        random_row_number = random.randrange(1, len(rows)-1)
+
+    data = {
+              "data1": rows[random_row_number][0],
+              "data2":  rows[random_row_number][1]
+            }
+    headers = {'Content-Type': 'application/json'}
+    response = self.client.post("/data",  json=data, headers=headers)
+
+    assert response.status_code is 200, "Unexpected response code: " + response.status_code
+    assert response.elapsed < datetime.timedelta(seconds=2), "Request took more than 2 second"
+
 def post_data(self: TaskSet, data_size: int):
     """
     :param self: TaskSet object.
@@ -46,6 +67,18 @@ def post_data(self: TaskSet, data_size: int):
              "data1": random_string(data_size),
              "data2":  random_string(data_size)
         }
+    headers = {'Content-Type': 'application/json'}
+    response = self.client.post("/data",  json=data, headers=headers)
+
+    assert response.status_code is 200, "Unexpected response code: " + response.status_code
+    assert response.elapsed < datetime.timedelta(seconds=2), "Request took more than 2 second"
+
+def post_data_json(self: TaskSet):
+    """
+    :param self: TaskSet object.
+    """
+    with open(path.join("data","data_json.json")) as json_file:
+        data = json.load(json_file)
     headers = {'Content-Type': 'application/json'}
     response = self.client.post("/data",  json=data, headers=headers)
 
@@ -81,6 +114,18 @@ class ApiTasks2(TaskSet):
     def post_big_data(self):
         post_data(self,3000)
 
+class ApiTasks3(TaskSet):
+    def on_start(self):
+       print("ApiTasks3 start.")
+
+    @task
+    def post_data_from_csv(self):
+        post_data_csv(self)
+
+    @task
+    def post_data_from_json(self):
+        post_data_json(self)
+
 
 class ApiUser1(HttpLocust):
     task_set = ApiTasks1
@@ -89,5 +134,10 @@ class ApiUser1(HttpLocust):
 
 class ApiUser2(HttpLocust):
     task_set = ApiTasks2
+    min_wait = 2000
+    max_wait = 5000
+
+class ApiUser3(HttpLocust):
+    task_set = ApiTasks3
     min_wait = 2000
     max_wait = 5000
