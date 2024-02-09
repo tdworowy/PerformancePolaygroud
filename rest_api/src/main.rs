dbde179ec4bef777752d3d8d4a07905378475f2f
Tomasz Dworowy <dworowytomasz@gmail.com>
2024-02-09 17:44:58 +0100

    add example rest api

---
+++ rest_api/src/main.rs
@@ -0,0 +1,22 @@
+use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+use rand::{distributions::Alphanumeric, Rng};
+
+#[get("/randStr")]
+async fn random_string() -> impl Responder {
+    let str_size: usize = rand::thread_rng().gen_range(100..5000);
+    let rand_str: String = rand::thread_rng()
+        .sample_iter(&Alphanumeric)
+        .take(str_size)
+        .map(char::from)
+        .collect();
+
+    HttpResponse::Ok().body(rand_str)
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().service(random_string))
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
+}
