9fc9cb2b298ba99d7dae2d0bcd934c8dc4894b1b
Tomasz Dworowy <dworowytomasz@gmail.com>
2024-02-10 16:13:21 +0100

    update:

--- rest_api/src/main.rs
+++ rest_api/src/main.rs
@@ -16,7 +16,7 @@ async fn random_string() -> impl Responder {
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| App::new().service(random_string))
-        .bind(("127.0.0.1", 8080))?
+        .bind(("0.0.0.0", 8080))?
         .run()
         .await
 }
