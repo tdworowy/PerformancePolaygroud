2cf839655c140f50537fef8eb9ae141df4c3e181
Tomasz Dworowy <dworowytomasz@gmail.com>
2024-02-12 17:57:28 +0100

    refactor

--- Locust/reporter.py
+++ Locust/reporter.py
@@ -4,7 +4,30 @@ import mpld3
 import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib.figure import Figure
+from matplotlib.axes import  Axes
 
+
+
+
+def plot_percentile(i:int, name:str,data_percentile:pd.DataFrame,axis:Axes):
+    plot1 = data_percentile.plot(x="Timestamp", ax=axis[i, 0])
+
+    plot1.set_facecolor('#B0C4DE')
+    plot1.set_title(f"{name} percentiles")
+
+    box = plot1.get_position()
+    plot1.set_position([box.x0, box.y0, box.width * 0.8, box.height])
+    plot1.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+
+def plot_failures(i:int, name:str,data_failures:pd.DataFrame,axis:Axes):
+    plot2 = data_failures.plot(x="Timestamp", ax=axis[i, 1])
+
+    plot2.set_facecolor('#B0C4DE')
+    plot2.set_title(f"{name} failures")
+
+    box = plot2.get_position()
+    plot2.set_position([box.x0, box.y0, box.width * 0.8, box.height])
+    plot2.legend(loc='center left', bbox_to_anchor=(1, 0.5))
 
 def generate_report(csv_file_prefix:str) -> Figure:
     data = pd.read_csv(f"{csv_file_prefix}.csv_stats_history.csv")
@@ -24,20 +47,8 @@ def generate_report(csv_file_prefix:str) -> Figure:
         data_percentile = _data[["Timestamp", "Total Max Response Time", "50%","66%","75%","80%","90%","95%","98%","99%","99.9%","99.99%","100%"]]
         data_failures = _data[["Timestamp", "Total Failure Count"]]
 
-        plot1 = data_percentile.plot(x="Timestamp", ax=axis[i,0])
-        plot1.set_facecolor('#B0C4DE')
-        plot1.set_title(f"{name} percentiles")
-        box = plot1.get_position()
-        plot1.set_position([box.x0, box.y0, box.width * 0.8, box.height])
-        plot1.legend(loc='center left', bbox_to_anchor=(1, 0.5))
-
-        plot2 = data_failures.plot(x="Timestamp", ax=axis[i, 1])
-        plot2.set_facecolor('#B0C4DE')
-        plot2.set_title(f"{name} failures")
-        box = plot2.get_position()
-        plot2.set_position([box.x0, box.y0, box.width * 0.8, box.height])
-        plot2.legend(loc='center left', bbox_to_anchor=(1, 0.5))
-
+        plot_percentile(i,name, data_percentile,axis)
+        plot_failures(i,name, data_failures,axis)
 
     return figure
 
