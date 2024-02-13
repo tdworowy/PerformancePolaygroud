0494acf153b37b2783f7458cf443a344f7d6aaa9
Tomasz Dworowy <dworowytomasz@gmail.com>
2024-02-13 09:52:46 +0100

    update

--- Locust/reporter.py
+++ Locust/reporter.py
@@ -7,33 +7,42 @@ from matplotlib.figure import Figure
 from matplotlib.axes import  Axes
 
 
-
-
 def plot_percentile(i:int, name:str,data_percentile:pd.DataFrame,axis:Axes):
-    plot1 = data_percentile.plot(x="Timestamp", ax=axis[i, 0])
+    plot = data_percentile.plot(x="Timestamp", ax=axis[i, 0])
 
-    plot1.set_facecolor('#B0C4DE')
-    plot1.set_title(f"{name} percentiles")
+    plot.set_facecolor('#B0C4DE')
+    plot.set_title(f"{name} percentiles")
 
-    box = plot1.get_position()
-    plot1.set_position([box.x0, box.y0, box.width * 0.8, box.height])
-    plot1.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+    box = plot.get_position()
+    plot.set_position([box.x0, box.y0, box.width * 0.8, box.height])
+    plot.legend(loc='center left', bbox_to_anchor=(1, 0.5))
 
 def plot_failures(i:int, name:str,data_failures:pd.DataFrame,axis:Axes):
-    plot2 = data_failures.plot(x="Timestamp", ax=axis[i, 1])
+    plot = data_failures.plot(x="Timestamp", ax=axis[i, 1])
 
-    plot2.set_facecolor('#B0C4DE')
-    plot2.set_title(f"{name} failures")
+    plot.set_facecolor('#B0C4DE')
+    plot.set_title(f"{name} failures")
 
-    box = plot2.get_position()
-    plot2.set_position([box.x0, box.y0, box.width * 0.8, box.height])
-    plot2.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+    box = plot.get_position()
+    plot.set_position([box.x0, box.y0, box.width * 0.8, box.height])
+    plot.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+
+
+def plot_distribution(i:int, name:str,data_dist:pd.DataFrame,axis:Axes):
+    plot = data_dist.plot.hist(y="Total Request Count",ax=axis[i, 2])
+
+    plot.set_facecolor('#B0C4DE')
+    plot.set_title(f"{name} request distribution")
+
+    box = plot.get_position()
+    plot.set_position([box.x0, box.y0, box.width * 0.8, box.height])
+    plot.legend(loc='center left', bbox_to_anchor=(1, 0.5))
 
 def generate_report(csv_file_prefix:str) -> Figure:
     data = pd.read_csv(f"{csv_file_prefix}.csv_stats_history.csv")
     names = data["Name"].unique()
 
-    figure, axis = plt.subplots(nrows=len(names), ncols=2, constrained_layout=True)
+    figure, axis = plt.subplots(nrows=len(names), ncols=3, constrained_layout=True)
 
     x, y = figure.get_size_inches()
     figure.set_figwidth(x * 5)
@@ -46,9 +55,11 @@ def generate_report(csv_file_prefix:str) -> Figure:
         _data = data.loc[data["Name"] == name]
         data_percentile = _data[["Timestamp", "Total Max Response Time", "50%","66%","75%","80%","90%","95%","98%","99%","99.9%","99.99%","100%"]]
         data_failures = _data[["Timestamp", "Total Failure Count"]]
+        data_dist = _data[["Total Request Count", "Total Max Response Time", "50%","66%","75%","80%","90%","95%","98%","99%","99.9%","99.99%","100%"]]
 
         plot_percentile(i,name, data_percentile,axis)
         plot_failures(i,name, data_failures,axis)
+        plot_distribution(i,name, data_dist,axis)
 
     return figure
 
