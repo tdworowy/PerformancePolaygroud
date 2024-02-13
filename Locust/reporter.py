54f545e5ef485d486bf5bb1d7ba0f8da3831046c
Tomasz Dworowy <dworowytomasz@gmail.com>
2024-02-13 13:01:28 +0100

    update

--- Locust/reporter.py
+++ Locust/reporter.py
@@ -7,6 +7,7 @@ from matplotlib.figure import Figure
 from matplotlib.axes import  Axes
 
 
+
 def plot_percentile(i:int, name:str,data_percentile:pd.DataFrame,axis:Axes):
     plot = data_percentile.plot(x="Timestamp", ax=axis[i, 0])
 
@@ -27,12 +28,11 @@ def plot_failures(i:int, name:str,data_failures:pd.DataFrame,axis:Axes):
     plot.set_position([box.x0, box.y0, box.width * 0.8, box.height])
     plot.legend(loc='center left', bbox_to_anchor=(1, 0.5))
 
-
 def plot_distribution(i:int, name:str,data_dist:pd.DataFrame,axis:Axes):
-    plot = data_dist.plot.hist(y="Total Request Count",ax=axis[i, 2])
+    plot = data_dist.plot.hist(ax=axis[i, 2], density=True,)
 
     plot.set_facecolor('#B0C4DE')
-    plot.set_title(f"{name} request distribution")
+    plot.set_title(f"{name} Total Max Response Time")
 
     box = plot.get_position()
     plot.set_position([box.x0, box.y0, box.width * 0.8, box.height])
@@ -55,7 +55,7 @@ def generate_report(csv_file_prefix:str) -> Figure:
         _data = data.loc[data["Name"] == name]
         data_percentile = _data[["Timestamp", "Total Max Response Time", "50%","66%","75%","80%","90%","95%","98%","99%","99.9%","99.99%","100%"]]
         data_failures = _data[["Timestamp", "Total Failure Count"]]
-        data_dist = _data[["Total Request Count", "Total Max Response Time", "50%","66%","75%","80%","90%","95%","98%","99%","99.9%","99.99%","100%"]]
+        data_dist = _data[["Total Max Response Time"]]
 
         plot_percentile(i,name, data_percentile,axis)
         plot_failures(i,name, data_failures,axis)
