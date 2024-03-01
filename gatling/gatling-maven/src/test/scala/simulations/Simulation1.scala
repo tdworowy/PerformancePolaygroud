141d4795dae8f020fbd62bbc85ef296f5c280965
Tomasz Dworowy <dworowytomasz@gmail.com>
2024-03-01 11:34:01 +0100

    fixes

--- gatling/gatling-maven/src/test/scala/simulations/Simulation1.scala
+++ gatling/gatling-maven/src/test/scala/simulations/Simulation1.scala
@@ -1,4 +1,4 @@
-package gatling.maven.simulation 
+package simulations
 
 import scala.concurrent.duration._
 import io.gatling.core.Predef._
@@ -6,18 +6,20 @@ import io.gatling.http.Predef._
 import io.gatling.core.structure.ScenarioBuilder
 import io.gatling.http.protocol.HttpProtocolBuilder
 
-class Example1 extends Simulation {
- val generateRandomStr = (range:Int) => scala.util.Random.alphanumeric.take(range).mkString
-   
- val httpProtocol = http
+import scala.language.postfixOps
+
+class Simulation1 extends Simulation {
+ val generateRandomStr: Int => String = (range:Int) => scala.util.Random.alphanumeric.take(range).mkString
+
+ val httpProtocol: HttpProtocolBuilder = http
     .baseUrl("http://192.168.50.241:8080") 
- val scn = scenario("GetSimulation")
+ val scn: ScenarioBuilder = scenario("GetSimulation")
     .exec(http("Get Random String") 
       .get("/randStr")
       .check(status.is(200)))
     .pause(2)
-  
- val scn1 = scenario("PostSimulation")
+
+ val scn1: ScenarioBuilder = scenario("PostSimulation")
   .exec(session => {
     val field1 = generateRandomStr(20)
     val field2 = generateRandomStr(20)
@@ -30,10 +32,11 @@ class Example1 extends Simulation {
       .body(StringBody(session => s"""{"field1": "${session("field1").as[String]}","field2": "${session("field2").as[String]}"}""")).asJson
       .check(status.is(200)))
     .pause(2)  
-    
+
     setUp( 
     scn.inject(rampUsers(10) during(30 seconds)),
     scn1.inject(constantConcurrentUsers(10) during(30 seconds))
   ).protocols(httpProtocol)
 }
-// TODO 'not found' errros in vscodium, metals seems to don't work correctly with gatling 
+// TODO 'not found' errors in vscodium, metals seems to don't work correctly with gatling
+// works ok in IDEA
