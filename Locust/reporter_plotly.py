cc16136b1b01fbb8ea460d2920e6b604c986764a
Tomasz Dworowy <dworowytomasz@gmail.com>
2024-03-02 11:27:10 +0100

    update

--- Locust/reporter_plotly.py
+++ Locust/reporter_plotly.py
@@ -9,7 +9,7 @@ import plotly.express as px
 
 
 
-def plot_percentile( name:str,data_percentile:pd.DataFrame):
+def plot_percentile_timestamps( name:str,data_percentile:pd.DataFrame):
     fig = go.Figure()
     timestamps = list(map(lambda timestamp: datetime.fromtimestamp(timestamp).strftime("%H:%M:%S"), data_percentile["Timestamp"]))
 
@@ -17,12 +17,28 @@ def plot_percentile( name:str,data_percentile:pd.DataFrame):
         fig.add_trace(go.Scatter(x=timestamps, y=data_percentile[percentile], mode='lines', name=percentile))
 
     fig.update_layout(title=f"{name} percentiles",
-                      xaxis_title='Timestamp',
+                      xaxis_title="Timestamp",
                       yaxis_title='Response Time',
                       xaxis=dict(
                           tickmode='linear',
                           tick0=1,
                           dtick=15
+                      )
+                      )
+    fig.show()
+
+def plot_percentile_other( name:str,data_percentile:pd.DataFrame, x_axis):
+    fig = go.Figure()
+    for percentile in filter(lambda  c: c != x_axis,data_percentile.columns):
+        fig.add_trace(go.Scatter(x=data_percentile[x_axis], y=data_percentile[percentile], mode='lines', name=percentile))
+
+    fig.update_layout(title=f"{name} percentiles",
+                      xaxis_title=x_axis,
+                      yaxis_title='Response Time',
+                      xaxis=dict(
+                          tickmode='linear',
+                          tick0=5,
+                          dtick=5
                       )
                       )
     fig.show()
@@ -52,14 +68,20 @@ def plot_distribution(name:str,data_dist:pd.DataFrame):
 
 def generate_report(csv_file:str) :
     data = pd.read_csv(f"{csv_file}")
+    data = data.loc[data["User Count"] > 0]
     names = data["Name"].unique()
+
     for name in names:
         _data = data.loc[data["Name"] == name]
-        data_percentile = _data[["Timestamp", "Total Max Response Time", "50%","66%","75%","80%","90%","95%","98%","99%","99.9%","99.99%","100%"]]
+        data_percentile_timestamp = _data[["Timestamp", "Total Max Response Time", "50%","66%","75%","80%","90%","95%","98%","99%","99.9%","99.99%","100%"]]
+        data_percentile_user_count = _data[
+            ["User Count", "Total Max Response Time", "50%", "66%", "75%", "80%", "90%", "95%", "98%", "99%", "99.9%",
+             "99.99%", "100%"]]
         data_failures = _data[["Timestamp", "Total Failure Count"]]
         data_dist = _data[["Total Max Response Time"]]
 
-        plot_percentile(name, data_percentile)
+        plot_percentile_timestamps(name, data_percentile_timestamp)
+        plot_percentile_other(name, data_percentile_user_count, "User Count") # TODO it looks wierd
         plot_failures(name, data_failures)
         plot_distribution(name, data_dist)
 
